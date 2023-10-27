2f42c48c6e0ec688f195411b2ada7946eaf1dcde
SinanCPH <cph-sf221@cphbusiness.dk>
2023-10-27 12:09:48 +0200

    Conflict update

---
+++ src/main/java/org/example/Conflict.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Conflict {
+    public Conflict(){
+
+    }
+    public void mergeConflict(){
+        System.out.println("Sinan");
+    }
+}
